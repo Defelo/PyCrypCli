24f19e07f330934719476d5507b59df17964eb7e
Defelo <elodef42@gmail.com>
2022-03-05 22:08:03 +0100

    Fixed ResourceUsage

--- PyCrypCli/game_objects/resource_usage.py
+++ PyCrypCli/game_objects/resource_usage.py
@@ -4,20 +4,20 @@ from PyCrypCli.game_objects.game_object import GameObject
 class ResourceUsage(GameObject):
     @property
     def cpu(self) -> float:
-        return self._data.get("cpu")
+        return min(1, self._data["usage_cpu"] / self._data["performance_cpu"])
 
     @property
     def ram(self) -> float:
-        return self._data.get("ram")
+        return min(1, self._data["usage_ram"] / self._data["performance_ram"])
 
     @property
     def gpu(self) -> float:
-        return self._data.get("gpu")
+        return min(1, self._data["usage_gpu"] / self._data["performance_gpu"])
 
     @property
     def disk(self) -> float:
-        return self._data.get("disk")
+        return min(1, self._data["usage_disk"] / self._data["performance_disk"])
 
     @property
     def network(self) -> float:
-        return self._data.get("network")
+        return min(1, self._data["usage_network"] / self._data["performance_network"])
